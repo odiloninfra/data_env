266e2e0c9305baf1b1358d4e46bc8bd5b4cdfe79
odiloninfra <@odilon-cloud>
2023-12-28 13:53:11 +0200

    commit files

--- dev_migrations/V1__create_book.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE book
-(
-    id                INTEGER NOT NULL,
-    title             VARCHAR(255),
-    author_first_name VARCHAR(255),
-    author_last_name  VARCHAR(255),
-    author_email      VARCHAR(255),
-    CONSTRAINT pk_book PRIMARY KEY (id)
-);
